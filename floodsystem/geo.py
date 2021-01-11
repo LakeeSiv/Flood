c3071195e9009dc4ee2f9eb37b46ec295e98f9dd
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-01-11 14:02:44 +0000

    created stations_by_distance function

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,21 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+
+
+def stations_by_distance(stations, p):
+    """ Function to calcuate the distannce between a station
+    and the coordinate p, and returns a list of the station and
+    the distance"""
+
+    resulting_arr = []
+
+    for station in stations:
+        coord = station.coordinate
+        distance = haversine(coord,p)
+
+        resulting_arr.append((station, distance))
+
+    return resulting_arr
+
