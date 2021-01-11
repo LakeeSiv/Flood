501b6924b11f5066ca64d1091f391e94e0fdb7e0
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-01-11 16:07:56 +0000

    added function for task 1c

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -24,3 +24,20 @@ def stations_by_distance(stations, p):
         resulting_arr.append((station, distance))
 
     return sorted_by_key(resulting_arr, 1, reverse=False)
+
+
+def stations_within_radius(stations, centre, r):
+    """
+    Function returns a list of stations which are with a radius r of
+    the centre coordinate
+    """
+    stations_distances = stations_by_distance(stations, centre)
+    result_arr = []
+
+    for tuple in stations_distances:
+        if tuple[1] <= r:
+            result_arr.append(tuple[0].name)
+        else:
+            break  # can break since the list is ordered by distance
+
+    return result_arr
