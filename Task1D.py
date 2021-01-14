0a49afa8031315a538cbb34170c39d3afca72170
Frank McMullan <fm528@cam.ac.uk>
2021-01-14 02:47:00 +0000

    Task1DFormatToRequirementsAutopep8

--- Task1D.py
+++ Task1D.py
@@ -3,19 +3,20 @@
 from floodsystem.geo import stations_by_river, rivers_with_station
 from floodsystem.stationdata import build_station_list
 
+
 def run():
     '''Requrements for Task 1D'''
 
-    #Builds Stations List
+    # Builds Stations List
     stations = build_station_list()
 
-    #Compiles a list of rivers. prints the length and first ten
+    # Compiles a list of rivers. prints the length and first ten
     riversWithStation = rivers_with_station(stations)
     print(len(riversWithStation))
     sortedList = sorted(riversWithStation)
     print(sortedList[:10])
 
-    #Compiles a list of stations sorted by river and prints a list of stations on three rivers in particular
+    # Compiles a list of stations sorted by river and prints a list of stations on three rivers in particular
     stationsByRiver = stations_by_river(stations)
     riverAire = list()
     riverCam = list()
