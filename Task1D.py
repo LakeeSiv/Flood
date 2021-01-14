ef498540f39757bc3b678103c77811fe0cca9433
Frank McMullan <fm528@cam.ac.uk>
2021-01-14 02:45:21 +0000

    Task1DFormatToRequirements

--- Task1D.py
+++ Task1D.py
@@ -3,27 +3,37 @@
 from floodsystem.geo import stations_by_river, rivers_with_station
 from floodsystem.stationdata import build_station_list
 
-stations = build_station_list()
+def run():
+    '''Requrements for Task 1D'''
 
-riversWithStation = rivers_with_station(stations)
-print(len(riversWithStation))
-sortedList = sorted(riversWithStation)
-print(sortedList[:10])
+    #Builds Stations List
+    stations = build_station_list()
 
-stationsByRiver = stations_by_river(stations)
-riverAire = list()
-riverCam = list()
-riverThames = list()
-for station in stationsByRiver['River Aire']:
-    riverAire.append(station.name)
-    riverAire.sort()
+    #Compiles a list of rivers. prints the length and first ten
+    riversWithStation = rivers_with_station(stations)
+    print(len(riversWithStation))
+    sortedList = sorted(riversWithStation)
+    print(sortedList[:10])
 
-for station in stationsByRiver['River Cam']:
-    riverCam.append(station.name)
-    riverCam.sort()
+    #Compiles a list of stations sorted by river and prints a list of stations on three rivers in particular
+    stationsByRiver = stations_by_river(stations)
+    riverAire = list()
+    riverCam = list()
+    riverThames = list()
+    for station in stationsByRiver['River Aire']:
+        riverAire.append(station.name)
+        riverAire.sort()
 
-for station in stationsByRiver['River Thames']:
-    riverThames.append(station.name)
-    riverThames.sort()
+    for station in stationsByRiver['River Cam']:
+        riverCam.append(station.name)
+        riverCam.sort()
 
-print(riverAire, '\n', riverCam, '\n', riverThames)
+    for station in stationsByRiver['River Thames']:
+        riverThames.append(station.name)
+        riverThames.sort()
+
+    print(riverAire, '\n', riverCam, '\n', riverThames)
+
+
+if __name__ == "__main__":
+    run()
