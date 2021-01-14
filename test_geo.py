67daba6ca90a90fd06bd5c199a38944db23a5137
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-01-14 13:22:23 +0000

    added test for stations_within_radius

--- test_geo.py
+++ test_geo.py
@@ -1,7 +1,30 @@
 """Tests the module Geo to ensure its functions return the desired information"""
 
-from floodsystem.geo import rivers_with_station, stations_by_river, rivers_by_station_number
+from floodsystem.geo import rivers_with_station, stations_by_river, rivers_by_station_number, stations_within_radius, stations_by_distance
 from floodsystem.station import MonitoringStation
+from floodsystem.stationdata import build_station_list
+
+
+# def test_stations_by_distance():
+
+def test_stations_within_radius():
+    """tests to see if the closest 10 stations within 10km of Cambridge City Centre
+        is correct to the representative output on the documentation"""
+
+    stations = build_station_list()
+    coord_camcity = (52.2053, 0.1218)
+
+    arr = stations_within_radius(stations, coord_camcity, 10)
+    sorted_arr = sorted(arr)  # sort alphabetically
+
+    assert sorted_arr == ['Bin Brook', 'Cambridge Baits Bite', "Cambridge Byron's Pool",
+                        'Cambridge Jesus Lock', 'Comberton', 'Dernford', 'Girton',
+                        'Haslingfield Burnt Mill', 'Lode', 'Oakington', 'Stapleford']
+
+
+
+
+
 
 
 def generate_test_station():
