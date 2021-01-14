05b99c5fabd387d0e1ef9513dfab765fa70fa8a7
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-01-14 16:30:06 +0000

    imporved  structure of task files

--- Task1B.py
+++ Task1B.py
@@ -1,10 +1,6 @@
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
 
-stations = build_station_list()
-coord_camcity = (52.2053, 0.1218)
-stations_list = stations_by_distance(stations, coord_camcity)
-
 
 def convert(x):
     """
@@ -25,9 +21,19 @@ def convert(x):
     return res_arr
 
 
-converted_list = convert(stations_list)
+def run():
 
-closest_10 = converted_list[:10]
-furthest_10 = converted_list[-10:]
+    stations = build_station_list()
+    coord_camcity = (52.2053, 0.1218)
+    stations_list = stations_by_distance(stations, coord_camcity)
 
-print(f"{closest_10}\n{furthest_10}")
+    converted_list = convert(stations_list)
+
+    closest_10 = converted_list[:10]
+    furthest_10 = converted_list[-10:]
+
+    print(f"{closest_10}\n{furthest_10}")
+
+
+if __name__ == "__main__":
+    run()
--- Task1F.py
+++ Task1F.py
@@ -1,9 +1,15 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.station import inconsistent_typical_range_stations
 
-stations = build_station_list()
-inconsistant_stations = inconsistent_typical_range_stations(stations)
 
-inconsistant_stations = sorted([station.name for station in inconsistant_stations])
+def run():
+    stations = build_station_list()
+    inconsistant_stations = inconsistent_typical_range_stations(stations)
 
-print(inconsistant_stations)
+    inconsistant_stations = sorted([station.name for station in inconsistant_stations])
+
+    print(inconsistant_stations)
+
+
+if __name__ == "__main__":
+    run()
