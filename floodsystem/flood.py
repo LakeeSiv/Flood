50ca5b61727397272d7e2b99b41949d48d007679
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-01-14 17:34:09 +0000

    fixed stations_level_over_thershold function

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -4,11 +4,17 @@ from .station import consistant_typical_range_stations
 def stations_level_over_threshold(stations: list, tol: float) -> list:
     """function takes in stations and returns a list of tuples contating station and
     relative water lever where the relative water level greater than tol """
-    stations = consistant_typical_range_stations(stations)
+
+    stations = consistant_typical_range_stations(stations)  # gets consistant stations
+
     res_list = []
 
     for station in stations:
-        if station.relative_water_level() > tol:
-            res_list.append((station, station.relative_water_level()))
+
+        rel_level = station.relative_water_level()
+
+        if rel_level is not None:  # ensures water level is not None
+            if rel_level > tol:
+                res_list.append((station, rel_level))
 
     return res_list
