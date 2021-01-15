f9567704b1fb40ca66cb402c9bf3e389fd55a80e
Frank McMullan <fm528@cam.ac.uk>
2021-01-15 23:16:06 +0000

    FixedImplementationIssues

--- Task2C.py
+++ Task2C.py
@@ -1,11 +1,12 @@
 from floodsystem.flood import stations_highest_rel_level
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 
 
 stations = build_station_list()
 
 
 def run():
+    update_water_levels(stations)
     topStations = stations_highest_rel_level(stations, 10)
     for i in topStations:
         print('{}  {}'.format(i.name, i.relative_water_level()))
---
+++ Task2E.py
@@ -0,0 +1,27 @@
+from floodsystem import datafetcher, flood, plot, station, stationdata
+from datetime import timedelta
+
+def run():
+    stations = stationdata.build_station_list()
+    stationdata.update_water_levels(stations)
+    stations = station.consistant_typical_range_stations(stations)
+
+    top5Stations = flood.stations_highest_rel_level(stations, 5)
+    top5StationsDates = []
+    top5StationsLevels = []
+    for top5station in top5Stations:
+        stationDates, stationLevels = datafetcher.fetch_measure_levels(top5station.measure_id, timedelta(days = 10))
+        top5StationsDates.append(stationDates)
+        top5StationsLevels.append(stationLevels)
+
+    plot.plot_water_levels(top5Stations, top5StationsDates, top5StationsLevels)
+    
+
+    
+
+
+if __name__ == "__main__":
+    run()
+        
+    
+    
