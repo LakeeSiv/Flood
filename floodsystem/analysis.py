93238643e6e96ffab8afd58cc4e0142223569fd6
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-01-18 16:23:03 +0000

    added warnings if degree is too high

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,14 +1,21 @@
 import numpy as np
 from matplotlib.dates import date2num
-from datetime import datetime
+import warnings
 
 
-def polyfit(dates :list, levels :list, p: int):
+def polyfit(dates: list, levels: list, p: int):
+    """Function that takes in the dates, level and degree, and polyfits
+    the data to the p th degree, returns a numpy.poly1d object and the dateshift"""
 
-    dates = date2num(dates)
-    date_shift = dates[0]
-    p_coeff = np.polyfit(dates - date_shift, levels , p)
-    poly = np.poly1d(p_coeff)
+    # warning if polynomial degree is too high
+    with warnings.catch_warnings():
+        warnings.filterwarnings("error")
+        try:
+            dates = date2num(dates)
+            date_shift = dates[0]
+            p_coeff = np.polyfit(dates - date_shift, levels, p)
+            poly = np.poly1d(p_coeff)
+            return poly, date_shift
 
-    return poly, date_shift
-
+        except np.RankWarning:
+            warnings.warn("Warning the degree is too high for the size of data given.\nPlease use a lower degree")
