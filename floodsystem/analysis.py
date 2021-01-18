cb87bb116f924628bab3326cbf9d9a5ce821bd9a
Frank McMullan <fm528@cam.ac.uk>
2021-01-18 21:33:49 +0000

    addedSortBySeverityFunction

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib.dates import date2num
 import warnings
+from .flood import stations_level_over_threshold
 
 
 def polyfit(dates: list, levels: list, p: int):
@@ -19,3 +20,33 @@ def polyfit(dates: list, levels: list, p: int):
 
         except np.RankWarning:
             warnings.warn("Warning the degree is too high for the size of data given.\nPlease use a lower degree")
+
+def sort_risk_level(stations):
+    '''Sorts stations into 4 levels of risk Severe, high, moderate and low with
+    relative river levels of 3.5, 2.5, 2 and 1 repectively.'''
+
+    stationsSevere = stations_level_over_threshold(stations, 3.5)
+    stationsHigh = stations_level_over_threshold(stations, 2.5)
+    stationsModerate = stations_level_over_threshold(stations, 2.0)
+    stationsLow = stations_level_over_threshold(stations, 1.0)
+    stationsSorted = []
+    for station in stations:
+        stationName = station.name
+        stationCoOrd = station.coord
+        stationTown = station.town
+        stationInfo = '{}, {}'.format(stationName, stationTown)
+        
+        if station in stationsSevere:
+            stationSeverity = 'S'
+        elif station in stationsHigh:
+            stationSeverity = 'H'
+        elif station in stationsModerate:
+            stationSeverity = 'M'
+        else:
+            stationSeverity = 'L'
+
+        stationData = (stationInfo, stationCoOrd, stationSeverity)
+        stationsSorted.append(stationData)
+            
+    return stationsSorted
+    
