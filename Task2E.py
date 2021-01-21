0409d3cef217c6021bbb77fd715a7a34099bd7de
Frank McMullan <fm528@cam.ac.uk>
2021-01-21 18:55:14 +0000

    autopep8again

--- Task2E.py
+++ Task2E.py
@@ -2,13 +2,12 @@ from floodsystem import datafetcher, flood, plot, station, stationdata
 from datetime import timedelta
 
 
-
 def run():
     stations = stationdata.build_station_list()
     stationdata.update_water_levels(stations)
     stations = station.consistant_typical_range_stations(stations)
 
-    top5Stations = flood.stations_highest_rel_level(stations, 2)
+    top5Stations = flood.stations_highest_rel_level(stations, 5)
     top5StationsDates = []
     top5StationsLevels = []
     for top5station in top5Stations:
@@ -17,9 +16,7 @@ def run():
         top5StationsLevels.append(stationLevels)
 
     plot.plot_water_levels(top5Stations, top5StationsDates, top5StationsLevels)
-    
 
 
 if __name__ == "__main__":
     run()
-    
